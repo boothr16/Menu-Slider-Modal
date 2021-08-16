eff680a9c6b2d56c237ba7d24551d04fd47d371f
Robert Booth <boothr16@gmail.com>
2021-08-16 16:51:42 -0400

    Added clarifying comments to script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const toggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-const modal = document.getElementById('modal');
+const toggle = document.getElementById('toggle'); // toggle button for side nav
+const close = document.getElementById('close'); // X in top right of modal window
+const open = document.getElementById('open'); // sign up button
+const modal = document.getElementById('modal'); // window that pops up after pressing sign up
 
 // Toggle nav
 toggle.addEventListener('click', () => 
@@ -17,3 +17,4 @@ close.addEventListener('click', () => modal.classList.remove('show-modal'));
 // Hide modal on outside click
 window.addEventListener('click', e => e.target == modal ?
 modal.classList.remove('show-modal') : false);
+// is the main window clicked when the modal window is up? if so, remove modal from screen
